1d99d3a091538f285f30adc5ae078f3aea2788e9
Juberlandio <Juberlandio.santana.js@gmail.com>
2024-01-17 11:32:56 -0300

    Commit Inicial

---
+++ script.js
@@ -0,0 +1,16 @@
+let Main = document.querySelector('main')
+let Footer = document.querySelector('footer')
+let menuOculto = document.getElementById('menu-oculto')
+let menuMobile = document.getElementById('menu-items')
+function abrir(){
+        menuMobile.classList.toggle('aberto')
+        menuOculto.classList.toggle('menu-animacao')
+        Main.classList.toggle('esconder')
+        Footer.classList.toggle('esconder')
+}
+
+const menu = document.querySelector('header');
+function ativarScroll(){
+  menu.classList.toggle('ativo', scrollY > 0)
+}
+window.addEventListener('scroll', ativarScroll)
