b437628d8cfdb70a5668bcf7a4b61ed01bce9fe8
juberlandio <juberlandio.santana.js@gmail.com>
2024-06-03 18:19:06 -0300

    Update js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
-import initScrollSuave from './modules/scroll-suave.js';
+// Função para o menu mobile - esconder as seções para a barra de pesquisa não fechar e bugar o Menu Mobile
+const Main = document.querySelector('main')
+const Footer = document.querySelector('footer')
+const menuOculto = document.getElementById('menu-oculto')
+const menuMobile = document.getElementById('menu-items')
 
-initScrollSuave();
+function abrir(){
+        menuMobile.classList.toggle('aberto')
+        menuOculto.classList.toggle('menu-animacao')
+        Main.classList.toggle('esconder')
+        Footer.classList.toggle('esconder')
+}
+
+// Função para o menu mobile - Scroll Menu Fixed
+function animacaoMenuFixed() {
+const menu = document.querySelector('header');
+
+function ativarScroll(){
+  menu.classList.toggle('ativo', scrollY > 0)
+}
+
+window.addEventListener('scroll', ativarScroll)
+}
+animacaoMenuFixed();
+
+
+function initScrollSuave() {
+const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+  section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSection);
+});
+}
+initScrollSuave()
